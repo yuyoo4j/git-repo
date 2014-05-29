22d0fbf96a3da182ad93a0d15bd93fe058f2675a
zhandulin <zhandulin@aspirecn.com>
2014-05-29 10:40:52 +0800

    s

--- java7maven/src/main/java/com/aspire/demo/SimpleCassandraClient.java
+++ java7maven/src/main/java/com/aspire/demo/SimpleCassandraClient.java
@@ -17,6 +17,7 @@ public class SimpleCassandraClient {
 			System.out.printf("Datacenter: %s; Host: %s; Rack: %s\n", host.getDatacenter(), host.getAddress(),
 					host.getRack());
 		}
+		//sss
 	}
 	
 	public Cluster getCluster() {
